86652be06607d736d2fca3ac11450a36b06dd0dc
Laurent Bindschaedler <laurent@bindschaedler.com>
2013-04-23 21:48:54 +0200

    Initial commit

---
+++ AuroraWatch/AuroraWatch/AuroraArea.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace AuroraWatch
+{
+    public class AuroraArea
+    {
+
+        public string RegionName { get; set; }
+        public string ActivityText { get; set; }
+        public string ForecastText { get; set; }
+
+    }
+}
---
+++ AuroraWatch/AuroraWatch/MainPage.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace AuroraWatch
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            Loaded += MainPageLoaded;
+            //Pivot.SelectionChanged += PivotChanged;
+        }
+
+        private static AuroraArea Europe = new AuroraArea { RegionName = "Europe", ActivityText = "LOW", ForecastText = "Europe..." };
+        private static AuroraArea America = new AuroraArea { RegionName = "America", ActivityText = "LOW", ForecastText = "America..." };
+        private static AuroraArea Alaska = new AuroraArea { RegionName = "Alaska", ActivityText = "LOW", ForecastText = "Alaska..." };
+        private static AuroraArea NorthPole = new AuroraArea { RegionName = "North Pole", ActivityText = "LOW", ForecastText = "NorthPole..." };
+        private static AuroraArea SouthPole = new AuroraArea { RegionName = "South Pole", ActivityText = "LOW", ForecastText = "SouthPole..." };
+
+        private AuroraArea[] Areas = new AuroraArea[] { Europe, America, Alaska, NorthPole, SouthPole };
+
+        private void SetDataContext(Grid grid, Info context)
+        {
+            grid.DataContext = context;
+        }
+
+        void MainPageLoaded(object sender, RoutedEventArgs e)
+        {
+            foreach(AuroraArea area in Areas) {
+                Pivot.Items.Add(new PivotItem 
+                {
+                    Header = area.RegionName,
+                    Content = new Grid()
+                });
+            }
+        }
+
+        /*void PivotChanged(object sender, RoutedEventArgs e)
+        {
+            var _pivotItem = Pivot.SelectedItem;
+
+            var _infoText = new Info
+            {
+                InfoText = "..."
+            };
+
+            if (_pivotItem == Pivot.)
+            {
+                _infoText.InfoText = "Europe";
+                SetDataContext(EuropeRoot, _infoText);
+            }
+            else if (_pivotItem == AmericaRoot)
+            {
+                _infoText.InfoText = "America";
+                SetDataContext(AmericaRoot, _infoText);
+            }
+            else if (_pivotItem == AlaskaRoot)
+            {
+                _infoText.InfoText = "Alaska";
+            }
+            else if (_pivotItem == NorthPoleRoot)
+            {
+                _infoText.InfoText = "North Pole";
+            }
+            else if (_pivotItem == SouthPoleRoot)
+            {
+                _infoText.InfoText = "South Pole";
+            }
+            else
+            {
+                // ignored
+                SetDataContext(EuropeRoot, _infoText);
+            }
+
+            
+        }*/
+    }
+}
